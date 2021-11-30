c859aed4218a0518b0bea5ba08e36b4c487b5086
Daria Babina <69820188+danidarya@users.noreply.github.com>
2021-11-30 13:34:45 +0300

    Add files via upload

---
+++ hw5/task1_tkinter_sympy.py
@@ -0,0 +1,417 @@
+from tkinter import *
+import math
+import heapq, random
+import time
+from sympy import Point, Polygon
+import numpy as np
+
+'''================= Your classes and methods ================='''
+
+def rotate(points, angle, center):
+    angle = math.radians(angle)
+    cos_val = math.cos(angle)
+    sin_val = math.sin(angle)
+    cx, cy = center
+    new_points = []
+
+    for x_old, y_old in points:
+        x_old -= cx
+        y_old -= cy
+        x_new = x_old * cos_val - y_old * sin_val
+        y_new = x_old * sin_val + y_old * cos_val
+        new_points.append((x_new+cx, y_new+cy))
+
+    return new_points
+
+def get_polygon_from_position(position) :
+    x,y,yaw = position
+    points = [(x - 50, y - 100), (x + 50, y - 100), (x + 50, y + 100), (x - 50, y + 100)] 
+    new_points = rotate(points, yaw * 180 / math.pi, (x,y))
+    return Polygon(*list(map(Point, new_points)))
+
+def get_polygon_from_obstacle(obstacle) :
+    points = [(obstacle[0], obstacle[1]), (obstacle[2], obstacle[3]), (obstacle[4], obstacle[5]), (obstacle[6], obstacle[7])] 
+    return Polygon(*list(map(Point, points)))
+
+
+def poly_collides(poly_lhs, poly_rhs) :
+    return poly_lhs.intersection(poly_rhs) or \
+        True in [poly_lhs.encloses_point(p) for p in poly_rhs.vertices]
+
+def collides(position, obstacle) :
+    return poly_collides(get_polygon_from_position(position),
+                         get_polygon_from_obstacle(obstacle))
+        
+
+class Window():
+        
+    '''================= Your Main Function ================='''
+    
+    
+    def go(self, event):
+        
+        for id in self.path_object_ids:
+            self.canvas.delete(id)
+        self.path_object_ids = []
+        start = self.get_start_position()
+        print('Start position', start)
+        path = self.a_star_search([start], self.path_cost, step=50, threshold=55)
+        self.path_object_ids = self.draw_path(path, final=True)
+        
+    def path_cost(self, position):
+ 
+        x_target, y_target, yaw_target = self.get_target_position()
+        x, y, yaw = position
+        x_dist, y_dist = x_target - x, y_target - y
+        distanse = math.sqrt(x_dist ** 2 + y_dist **2)
+        if y_dist == 0:
+            angle = math.pi if x_dist >= 0 else -math.pi
+        else:
+            angle = math.atan(abs(x_dist / y_dist))
+            if (x_dist >= 0) and (y_dist >= 0):
+                angle = math.pi - angle
+            elif (x_dist < 0) and (y_dist >= 0):
+                angle = angle - math.pi
+            elif (x_dist >= 0) and (y_dist < 0):
+                angle = angle
+            else:
+                angle = -angle
+        rotation = abs(angle - yaw_target)
+        if rotation > math.pi:
+            rotation = abs(rotation - 2 * math.pi)
+        return distanse + 350 * rotation
+
+
+    def get_next_points(self, point, step):
+        
+        next_points = []
+        x, y, yaw = point
+        angle = math.pi * step / 180 / 3
+        angles_to_add = [-angle, 0., angle]
+        for angle in angles_to_add:
+            yaw_new = yaw + angle
+            x_new = x + step * math.sin(yaw_new)
+            y_new = y - step * math.cos(yaw_new)
+            collision = False
+            for obstacle in self.get_obstacles():
+                if collides([x_new, y_new, yaw_new], obstacle) :
+                    collision = True
+                    break
+            if collision:
+                continue
+            next_points.append([x_new, y_new, yaw_new])
+        return next_points
+    
+    def a_star_search(self, path, path_cost, step, threshold):
+        
+        heap_lst, closed, path = [], set(), path
+        heapq.heappush(heap_lst, (path_cost(path[-1]), (len(path) - 1) * step, path))
+        while len(heap_lst) != 0:
+            _, _, path = heapq.heappop(heap_lst)
+            if path_cost(path[-1]) < threshold:
+                print('Target is found.')
+                break
+            for next_point in self.get_next_points(path[-1], step):
+                if (round(next_point[0], 1), round(next_point[1], 1), round(next_point[2], 1)) in closed:
+                    continue
+                next_path = path.copy()
+                next_path.append(next_point)
+                closed.add((round(next_point[0], 1), round(next_point[1], 1), round(next_point[2], 1)))
+                heapq.heappush(heap_lst, ((len(next_path) - 1) * step + path_cost(next_point), 
+                                          (len(next_path) - 1) * step,
+                                          next_path))
+            self.draw_path(path)
+        return path
+
+    def draw_path(self, path, final=False):
+        
+        ids = []
+        for point in path:
+            id = self.canvas.create_oval(point[0] - 5, point[1] - 5, point[0] + 5, point[1] + 5, fill='yellow')
+            ids.append(id)
+        self.root.update()
+        if final:
+            return ids
+        for id in ids:
+            self.canvas.delete(id)
+
+       
+    '''================= Interface Methods ================='''
+    
+    def get_obstacles(self) :
+        obstacles = []
+        potential_obstacles = self.canvas.find_all()
+        for i in potential_obstacles:
+            if (i > 2) :
+                coords = self.canvas.coords(i)
+                if coords:
+                    obstacles.append(coords)
+        return obstacles
+            
+            
+    def get_start_position(self) :
+        x,y = self.get_center(2) # Purple block has id 2
+        yaw = self.get_yaw(2)
+        return x,y,yaw
+    
+    def get_target_position(self) :
+        x,y = self.get_center(1) # Green block has id 1 
+        yaw = self.get_yaw(1)
+        return x,y,yaw 
+ 
+
+    def get_center(self, id_block):
+        coords = self.canvas.coords(id_block)
+        center_x, center_y = ((coords[0] + coords[4]) / 2, (coords[1] + coords[5]) / 2)
+        return [center_x, center_y]
+
+    def get_yaw(self, id_block):
+        center_x, center_y = self.get_center(id_block)
+        first_x = 0.0
+        first_y = -1.0
+        second_x = 1.0
+        second_y = 0.0
+        points = self.canvas.coords(id_block)
+        end_x = (points[0] + points[2])/2
+        end_y = (points[1] + points[3])/2
+        direction_x = end_x - center_x
+        direction_y = end_y - center_y
+        length = math.hypot(direction_x, direction_y)
+        unit_x = direction_x / length
+        unit_y = direction_y / length
+        cos_yaw = unit_x * first_x + unit_y * first_y 
+        sign_yaw = unit_x * second_x + unit_y * second_y
+        if (sign_yaw >= 0 ) :
+            return math.acos(cos_yaw)
+        else :
+            return -math.acos(cos_yaw)
+       
+    def get_vertices(self, id_block):
+        return self.canvas.coords(id_block)
+
+    '''=================================================='''
+
+    def rotate(self, points, angle, center):
+        angle = math.radians(angle)
+        cos_val = math.cos(angle)
+        sin_val = math.sin(angle)
+        cx, cy = center
+        new_points = []
+
+        for x_old, y_old in points:
+            x_old -= cx
+            y_old -= cy
+            x_new = x_old * cos_val - y_old * sin_val
+            y_new = x_old * sin_val + y_old * cos_val
+            new_points.append(x_new+cx)
+            new_points.append(y_new+cy)
+
+        return new_points
+
+    def start_block(self, event):
+        widget = event.widget
+        widget.start_x = event.x
+        widget.start_y = event.y
+
+    def in_rect(self, point, rect):
+        x_start, x_end = min(rect[::2]), max(rect[::2])
+        y_start, y_end = min(rect[1::2]), max(rect[1::2])
+
+        if x_start < point[0] < x_end and y_start < point[1] < y_end:
+            return True
+
+    def motion_block(self, event):
+        widget = event.widget
+
+        for i in range(1, 10):
+            if widget.coords(i) == []:
+                break
+            if self.in_rect([event.x, event.y], widget.coords(i)):
+                coords = widget.coords(i)
+                id = i
+                break
+
+        res_cords = []
+        try:
+            coords
+        except:
+            return
+
+        for ii, i in enumerate(coords):
+            if ii % 2 == 0:
+                res_cords.append(i + event.x - widget.start_x)
+            else:
+                res_cords.append(i + event.y - widget.start_y)
+
+        widget.start_x = event.x
+        widget.start_y = event.y
+        widget.coords(id, res_cords)
+        widget.center = ((res_cords[0] + res_cords[4]) / 2, (res_cords[1] + res_cords[5]) / 2)
+
+    def draw_block(self, points, color):
+        x = self.canvas.create_polygon(points, fill=color)
+        return x
+
+    def distance(self, x1, y1, x2, y2):
+        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+    def set_id_block(self, event):
+        widget = event.widget
+
+        for i in range(1, 10):
+            if widget.coords(i) == []:
+                break
+            if self.in_rect([event.x, event.y], widget.coords(i)):
+                coords = widget.coords(i)
+                id = i
+                widget.id_block = i
+                break
+
+        widget.center = ((coords[0] + coords[4]) / 2, (coords[1] + coords[5]) / 2)
+
+    def rotate_block(self, event):
+        angle = 0
+        widget = event.widget
+
+        if widget.id_block == None:
+            for i in range(1, 10):
+                if widget.coords(i) == []:
+                    break
+                if self.in_rect([event.x, event.y], widget.coords(i)):
+                    coords = widget.coords(i)
+                    id = i
+                    widget.id_block == i
+                    break
+        else:
+            id = widget.id_block
+            coords = widget.coords(id)
+
+        wx, wy = event.x_root, event.y_root
+        try:
+            coords
+        except:
+            return
+
+        block = coords
+        center = widget.center
+        x, y = block[2], block[3]
+
+        cat1 = self.distance(x, y, block[4], block[5])
+        cat2 = self.distance(wx, wy, block[4], block[5])
+        hyp = self.distance(x, y, wx, wy)
+
+        if wx - x > 0: angle = math.acos((cat1**2 + cat2**2 - hyp**2) / (2 * cat1 * cat2))
+        elif wx - x < 0: angle = -math.acos((cat1**2 + cat2**2 - hyp**2) / (2 * cat1 * cat2))
+
+        new_block = self.rotate([block[0:2], block[2:4], block[4:6], block[6:8]], angle, center)
+        self.canvas.coords(id, new_block)
+
+    def delete_block(self, event):
+        widget = event.widget.children["!canvas"]
+
+        for i in range(1, 10):
+            if widget.coords(i) == []:
+                break
+            if self.in_rect([event.x, event.y], widget.coords(i)):
+                widget.coords(i, [0,0])
+                break
+
+    def create_block(self, event):
+        block = [[0, 100], [100, 100], [100, 300], [0, 300]]
+
+        id = self.draw_block(block, "black")
+
+        self.canvas.tag_bind(id, "<Button-1>", self.start_block)
+        self.canvas.tag_bind(id, "<Button-3>", self.set_id_block)
+        self.canvas.tag_bind(id, "<B1-Motion>", self.motion_block)
+        self.canvas.tag_bind(id, "<B3-Motion>", self.rotate_block)
+
+    def make_draggable(self, widget):
+        widget.bind("<Button-1>", self.drag_start)
+        widget.bind("<B1-Motion>", self.drag_motion)
+
+    def drag_start(self, event):
+        widget = event.widget
+        widget.start_x = event.x
+        widget.start_y = event.y
+
+    def drag_motion(self, event):
+        widget = event.widget
+        x = widget.winfo_x() - widget.start_x + event.x + 200
+        y = widget.winfo_y() - widget.start_y + event.y + 100
+        widget.place(rely=0.0, relx=0.0, x=x, y=y)
+
+    def create_button_create(self):
+        button = Button(
+            text="New",
+            bg="#555555",
+            activebackground="blue",
+            borderwidth=0
+        )
+
+        button.place(rely=0.0, relx=0.0, x=200, y=100, anchor=SE, width=200, height=100)
+        button.bind("<Button-1>", self.create_block)
+
+    def create_green_block(self, center_x):
+        block = [[center_x - 50, 100],
+                 [center_x + 50, 100],
+                 [center_x + 50, 300],
+                 [center_x - 50, 300]]
+
+        id = self.draw_block(block, "green")
+
+        self.canvas.tag_bind(id, "<Button-1>", self.start_block)
+        self.canvas.tag_bind(id, "<Button-3>", self.set_id_block)
+        self.canvas.tag_bind(id, "<B1-Motion>", self.motion_block)
+        self.canvas.tag_bind(id, "<B3-Motion>", self.rotate_block)
+
+    def create_purple_block(self, center_x, center_y):
+        block = [[center_x - 50, center_y - 300],
+                 [center_x + 50, center_y - 300],
+                 [center_x + 50, center_y - 100],
+                 [center_x - 50, center_y - 100]]
+
+        id = self.draw_block(block, "purple")
+
+        self.canvas.tag_bind(id, "<Button-1>", self.start_block)
+        self.canvas.tag_bind(id, "<Button-3>", self.set_id_block)
+        self.canvas.tag_bind(id, "<B1-Motion>", self.motion_block)
+        self.canvas.tag_bind(id, "<B3-Motion>", self.rotate_block)
+
+    def create_button_go(self):
+        button = Button(
+            text="Go",
+            bg="#555555",
+            activebackground="blue",
+            borderwidth=0
+        )
+
+        button.place(rely=0.0, relx=1.0, x=0, y=200, anchor=SE, width=100, height=200)
+        button.bind("<Button-1>", self.go)
+
+    def run(self):
+        root = self.root
+
+        self.create_button_create()
+        self.create_button_go()
+        self.create_green_block(self.width/2)
+        self.create_purple_block(self.width/2, self.height)
+
+        root.bind("<Delete>", self.delete_block)
+
+        root.mainloop()
+        
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("")
+        self.width  = self.root.winfo_screenwidth()
+        self.height = self.root.winfo_screenheight()
+        self.root.geometry('{}x{}'.format(self.width, self.height))
+        self.canvas = Canvas(self.root, bg="#777777", height=self.height, width=self.width)
+        self.canvas.pack()
+
+        self.path_object_ids = []
+    
+if __name__ == "__main__":
+    run = Window()
+    run.run()
